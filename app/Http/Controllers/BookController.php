27bf0ba791259106ab1e4b4d2063672f54352fb5
Adrians <adrianszabitis@Opti-06.localdomain>
2026-02-26 11:32:10 +0200

    Added model for database controll and form data now saves to database

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
@@ -13,6 +14,13 @@ class BookController extends Controller
 
     public function store(Request $request)
     {
+        $validated = $request->validate([
+            'title' => 'string',
+            'author' => 'string',
+            'date' => 'string',
+            'description' => 'string|max:255'
+        ]);
+        Book::create($validated);
 
         return "success";
     }
